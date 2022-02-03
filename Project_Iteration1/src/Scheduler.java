cf693f9ca8700d00330d3199236fe1901bb59692
scsc2022 <98617968+scsc2022@users.noreply.github.com>
2022-02-02 21:38:34 -0500

    Add files via upload

---
+++ Project_Iteration1/src/Scheduler.java
@@ -0,0 +1,59 @@
+/**
+ * @author schararaislam
+ *
+ */
+public class Scheduler implements Runnable {
+	private boolean empty = false; //getting message
+	private Object messagelock = new Object();
+	private Thread messagehandler;
+	private Thread FloorSubsystemThread;
+	private Thread ElevatorSystemThread;
+	
+	
+	public Scheduler() {
+		messagehandler = new Thread(messagehandler, "scheduler message");
+		messagehandler.start();
+		
+		FloorSubsystemThread = new Thread(this.FloorSubsystemThread);
+		FloorSubsystemThread.start();
+		
+		ElevatorSystemThread = new Thread(this.ElevatorSystemThread);
+		ElevatorSystemThread.start();
+	}
+	
+	public static void main(String[] args) {
+		Scheduler s1 = new Scheduler();
+		
+	}
+	
+	
+/*
+	public synchronized void FloorSubsystem() {
+		while (empty) {
+			try {
+				this.wait();
+			} catch (InterruptedException e) {
+				return;
+			}
+		
+		notifyAll();
+		}
+
+	}
+	*/
+	/*
+	public synchronized void ElevatorSystem() {
+		while (empty) {
+			try {
+				this.wait();
+			} catch (InterruptedException e) {
+				return;
+			}
+		
+		notifyAll();
+	}
+	*/
+	
+	
+
+}
