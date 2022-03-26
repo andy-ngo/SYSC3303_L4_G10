b6bc2c8e843060a052c61f792d34cf681b402b6f
Karim Mahrous <karimmahrous@cmail.carleton.ca>
2022-03-25 20:40:17 -0400

    Add files via upload

---
+++ L4G10_milestone_4/Project_Iteration4/src/SchedulerState.java
@@ -0,0 +1,10 @@
+/***
+ * The purpose of this clase is to inform the state that the scheduler is in.
+ * 
+ * @author Karim Mahrous
+ *
+ */
+public enum SchedulerState {
+	STATE_1,
+    STATE_2;
+}
