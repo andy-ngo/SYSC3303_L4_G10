6091d536ae270d1651507748466fe6fdaf203f81
andy-ngo <andyngo03@gmail.com>
2022-03-27 14:39:48 -0400

    feat:javadoc edit

--- L4G10_milestone_4/Project_Iteration4/src/ElevatorMotor.java
+++ L4G10_milestone_4/Project_Iteration4/src/ElevatorMotor.java
@@ -1,6 +1,9 @@
 /*
  * Author: Andy Ngo
+ * Student ID: 101132278
+ * Version: 2.0V
  * 
+ * Description:
  * The purpose of this class is for the elevator motor movement and it is just a enumeration class that will be used by the elevator thread
  */
 
